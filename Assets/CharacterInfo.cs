3301c023defee6337658b60b742015c86f9f7c74
DanielJochem <daniel.jochem@gmail.com>
2016-08-16 21:24:32 +1000

    Added disabling of other characters including twin.

--- Assets/CharacterInfo.cs
+++ Assets/CharacterInfo.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
@@ -40,6 +39,22 @@ public class CharacterInfo : SingletonBehaviour<CharacterInfo> {
         if(SceneManager.GetActiveScene().name == "StartScene") {
             startGameButton.enabled = (chosenPlayerName.Length > 2 && chosenFriendName.Length > 2 && chosenPlayerCharacter != "" && chosenFriendCharacter != "") ? true : false;
             startGameButton.image.color = (!startGameButton.enabled) ? startGameButton.colors.disabledColor : startGameButton.colors.normalColor;
+
+            foreach(Button bttn in playerButtonList) {
+                if(bttn.GetComponent<Image>().raycastTarget == false) {
+                    bttn.GetComponent<Image>().raycastTarget = true;
+                }
+
+                ChangeSprite();
+            }
+
+            foreach(Button bttn in friendButtonList) {
+                if(bttn.GetComponent<Image>().raycastTarget == false) {
+                    bttn.GetComponent<Image>().raycastTarget = true;
+                }
+
+                ChangeSprite();
+            }
         }
     }
 
@@ -48,22 +63,30 @@ public class CharacterInfo : SingletonBehaviour<CharacterInfo> {
             chosenPlayerCharacter = button.name;
 
             foreach(Button bttn in playerButtonList) {
-                if(chosenPlayerCharacter != "")
-                    bttn.GetComponent<Image>().sprite = bttn.spriteState.disabledSprite;
+                if(chosenPlayerCharacter != "") {
+                    bttn.image.sprite = bttn.spriteState.disabledSprite;
+                    bttn.image.CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 1.0f, false, true);
+                }
 
-                if(bttn.name == chosenPlayerCharacter)
-                    bttn.GetComponent<Image>().sprite = bttn.spriteState.pressedSprite;
+                if(bttn.name == chosenPlayerCharacter) {
+                    bttn.image.sprite = bttn.spriteState.pressedSprite;
+                    bttn.image.CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 1.0f), 1.0f, false, true);
+                }
             }
 
         } else if(button.tag == "FriendButton") {
             chosenFriendCharacter = button.name;
 
             foreach(Button bttn in friendButtonList) {
-                if(chosenFriendCharacter != "")
+                if(chosenFriendCharacter != "") {
                     bttn.GetComponent<Image>().sprite = bttn.spriteState.disabledSprite;
+                    bttn.image.CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 0.5f, false, true);
+                }
 
-                if(bttn.name == chosenFriendCharacter)
+                if(bttn.name == chosenFriendCharacter) {
                     bttn.GetComponent<Image>().sprite = bttn.spriteState.pressedSprite;
+                    bttn.image.CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 1.0f), 0.5f, false, true);
+                }
             }
         }
     }
@@ -84,7 +107,7 @@ public class CharacterInfo : SingletonBehaviour<CharacterInfo> {
         SceneManager.LoadScene(1);
     }
 
-    public static string TidyCase(string sourceStr) {
+    static string TidyCase(string sourceStr) {
         sourceStr.Trim();
         if(!string.IsNullOrEmpty(sourceStr)) {
             char[] allCharacters = sourceStr.ToCharArray();
@@ -104,5 +127,45 @@ public class CharacterInfo : SingletonBehaviour<CharacterInfo> {
             return new string(allCharacters);
         }
         return sourceStr;
+    }
+
+    void ChangeSprite() {
+        switch(chosenPlayerCharacter) {
+            case "PJohn":
+                GameObject.Find("FJohn").GetComponent<Image>().CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 0.5f, false, true);
+                GameObject.Find("FJohn").GetComponent<Image>().raycastTarget = false;
+                break;
+            case "PSherlock":
+                GameObject.Find("FSherlock").GetComponent<Image>().CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 0.5f, false, true);
+                GameObject.Find("FSherlock").GetComponent<Image>().raycastTarget = false;
+                break;
+            case "PJoanne":
+                GameObject.Find("FJoanne").GetComponent<Image>().CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 0.5f, false, true);
+                GameObject.Find("FJoanne").GetComponent<Image>().raycastTarget = false;
+                break;
+            case "PSherley":
+                GameObject.Find("FSherley").GetComponent<Image>().CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 0.5f, false, true);
+                GameObject.Find("FSherley").GetComponent<Image>().raycastTarget = false;
+                break;
+        }
+
+        switch(chosenFriendCharacter) {
+            case "FJohn":
+                GameObject.Find("PJohn").GetComponent<Image>().CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 0.5f, false, true);
+                GameObject.Find("PJohn").GetComponent<Image>().raycastTarget = false;
+                break;
+            case "FSherlock":
+                GameObject.Find("PSherlock").GetComponent<Image>().CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 0.5f, false, true);
+                GameObject.Find("PSherlock").GetComponent<Image>().raycastTarget = false;
+                break;
+            case "FJoanne":
+                GameObject.Find("PJoanne").GetComponent<Image>().CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 0.5f, false, true);
+                GameObject.Find("PJoanne").GetComponent<Image>().raycastTarget = false;
+                break;
+            case "FSherley":
+                GameObject.Find("PSherley").GetComponent<Image>().CrossFadeColor(new Color(1.0f, 1.0f, 1.0f, 0.2f), 0.5f, false, true);
+                GameObject.Find("PSherley").GetComponent<Image>().raycastTarget = false;
+                break;
+        }
     }
 }
